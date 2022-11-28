5fb87210c268af34acbc4ba77cb4114e81126c1d
beagleoasis <beagleoasis@naver.com>
2022-11-28 21:52:06 +0900

    컨트롤러 생성 및 hello 테스트

--- src/main/java/com/mbti/mbtitest/MbtiTestApplication.java
+++ src/main/java/com/mbti/mbtitest/MbtiTestApplication.java
@@ -3,11 +3,14 @@ package com.mbti.mbtitest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication으로 인해 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동
 @SpringBootApplication
 public class MbtiTestApplication {
 
     public static void main(String[] args) {
+        // SpringApplication.run으로 WAS(Tomcat) 서버 실행
         SpringApplication.run(MbtiTestApplication.class, args);
+
     }
 
 }
