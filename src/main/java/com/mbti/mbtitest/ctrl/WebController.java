edbe0862bd2de326761ad0ba874744b49460ade4
Kimjeongmin <>
2022-12-31 10:30:25 +0900

    실서버 마지막 반영 전 커밋

--- src/main/java/com/mbti/mbtitest/ctrl/WebController.java
+++ src/main/java/com/mbti/mbtitest/ctrl/WebController.java
@@ -1,18 +1,11 @@
 package com.mbti.mbtitest.ctrl;
 
 
-import com.mbti.mbtitest.service.PostsService;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class WebController {
-
-    private PostsService postsService;
 
     @GetMapping("usedSkills")
     public String selectUsedSkills() {
